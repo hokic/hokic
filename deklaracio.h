596f7f68468f663631e6ef88e6871e2d1e39a3d8
hokic <laci.mihalyi@gmail.com>
2013-01-29 19:53:28 +0100

    Update deklaracio.h

--- deklaracio.h
+++ deklaracio.h
@@ -1,6 +1,10 @@
 #ifndef DEKLARACIO_H_BEILLESZTVE
 #define DEKLARACIO_H_BEILLESZTVE
 
+//MENU FUGGVENYEK DEKLARALASA
 int menu(void);
+void beallitasok(void);
+void dicsoseglista(void);
+void leiras(void);
 
 #endif

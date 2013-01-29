9c3605d0f06e489d14b3377521a9fdd4f729aa06
hokic <laci.mihalyi@gmail.com>
2013-01-29 10:09:45 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <SDL.h>
+#include <sdl_gfxprimitives.h>
+#include <SDL_ttf.h>
+#include <SDL_image.h>
+
+#include "deklaracio.h"
+
+
+int main(int argc, char *argv[]){
+
+    menu();
+
+    SDL_Quit();
+    return 0;
+}

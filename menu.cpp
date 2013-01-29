9c051acb625a66f2875ead8b98dbbde45b9e37b0
hokic <laci.mihalyi@gmail.com>
2013-01-29 19:48:59 +0100

    Update menu.cpp
    menu fuggvenyek konkretizalasa

--- menu.cpp
+++ menu.cpp
@@ -24,6 +24,14 @@ int menu(void){
     while(!quit){
         /*Kepek megjelenitese, majd a switch-case kombo a kattintasok vizsgalatara.*/
         
+        boxRGBA(screen, 0, 0, 500, 250, 255, 255, 255, 255);
+        
+        /* Szamomra erthetetlen modon egybol bezarodik a program, majd akinek lesz 
+         * otlete... Mindenesetre a grafikai resznek is neki kell majd kezdeni
+         * hogy ne visszafele kelljen dolgoni.*/
+        
+        SDL_Flip(screen);
+        
         SDL_WaitEvent (&event);
         switch (event.type){
             case SDL_QUIT:
@@ -36,3 +44,23 @@ int menu(void){
         
     }
 }
+
+void beallitasok(void){
+    /*Ha csinalunk opciokat, akkor annak a fuggveny*/
+    
+}
+
+void dicsoseglista(void){
+    /* Dicsoseglista fuggvenye. Ezt nagy valoszinuseggel itt is
+     * lancolt listaval kell majd kivitelezni, abban pedig mostansag profi
+     * vagyok. A jatekban merni fogjuk az idot, illetve a golok szamat. Lehetne
+     * nehezsegi szinteket csinalni es akkor ezek alapjan lehetne rangsorolni./
+    
+}
+
+void leiras(void){
+    /*A jatek leirasat ebben a fuggvenyben tudjuk majd megcsinalni*/
+
+}
+
+/*Ha van otlet tovabbi menupontokhoz, akkor ide irjatok majd*/

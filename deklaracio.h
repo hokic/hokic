43e8bc328d222c7f1c49e12a10aad72607cfb565
hokic <laci.mihalyi@gmail.com>
2013-01-29 21:45:48 +0100

    Update deklaracio.h

--- deklaracio.h
+++ deklaracio.h
@@ -1,14 +1,18 @@
 #ifndef DEKLARACIO_H_BEILLESZTVE
 #define DEKLARACIO_H_BEILLESZTVE
 
+#include <SDL.h>
+#include <SDL_ttf.h>
+
+
 //MENU FUGGVENYEK DEKLARALASA
-int menu(void);
+int menu(SDL_Event event, SDL_Surface *screen);
 void beallitasok(void);
 void dicsoseglista(void);
 void leiras(void);
 
 //JATEK FUGGVENYEK DEKLARALASA
-Uint32 idozit(Uint32 ms, void* param);
+//Uint32 idozit(Uint32 ms, void* param);
 void jatek(void);
 
 #endif

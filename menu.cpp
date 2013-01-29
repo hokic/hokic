23f0e86f072c7f7396a28d639817b1daa4627199
hokic <laci.mihalyi@gmail.com>
2013-01-29 10:17:37 -0800

    Create menu.cpp

---
+++ menu.cpp
@@ -0,0 +1,12 @@
+#include "deklaracio.h"
+
+int menu(void){
+    SDL_Event event;
+    SDL_Surface *screen;
+
+    /* Menu megjelenitese, illetve a kattintasok vizsgalata.
+     * Szerintem ugy lenne a jo, ha a fuggvenyek minel kevesebb
+     * parameterrel rendelkeznenek, mert azoknak a kiertekelese
+     * plusz teljesitmeny.*/
+
+}
